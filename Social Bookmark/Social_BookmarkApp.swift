6d4d91af073dbcf242f8d9cd7cd065022177cb6f
unalkoseoglu <unallkoseoglu@gmail.com>
2025-12-10 16:09:06 +0300

    Initial Commit

---
+++ Social Bookmark/Social_BookmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Social_BookmarkApp.swift
+//  Social Bookmark
+//
+//  Created by Ünal Köseoğlu on 10.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct Social_BookmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Social BookmarkUITests/Social_BookmarkUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Social_BookmarkUITestsLaunchTests.swift
+//  Social BookmarkUITests
+//
+//  Created by Ünal Köseoğlu on 10.12.2025.
+//
+
+import XCTest
+
+final class Social_BookmarkUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
